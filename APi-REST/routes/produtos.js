1aefeb1a98e3d4dfc071c5e38a330a9801e659e3
Vinícius Oliveira <vinicius_rr1@hotmail.com>
2021-04-07 20:46:09 -0300

    Contexto de rotsa (Exemplo)

---
+++ APi-REST/routes/produtos.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/', (req, res, next) => {
+    res.status(200).send({
+        mensagem: 'Usando o GET junto com a rota de produtos'
+    });
+});
+
+router.post('/', (req, res, next) => {
+    res.status(201).send({
+        mensagem: 'Usando o POST dentro da rota de produtos'
+    });
+});
+
+router.get('/:id_produto', (req, res, next) => {
+    const id = req.params.id_produto
+
+    if(id === 'especial'){
+        res.status(200).send({
+            mensagem : 'Você descobrio o ID especial',
+            id: id
+        });
+    }else{
+        res.status(200).send({
+            mensagem: 'Você passou um ID',
+            id: id
+        });
+    }
+
+});
+
+module.exports = router;
